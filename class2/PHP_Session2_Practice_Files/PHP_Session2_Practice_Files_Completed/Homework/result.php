9deb439c412460effbfbcdd1eccc8ae3ffbdc0b6
TJ Nicolaides <tjnicolaides@gmail.com>
2014-06-07 00:16:52 -0400

    Switching doctypes and changing indentation - session 2

--- class2/PHP_Session2_Practice_Files/PHP_Session2_Practice_Files_Completed/Homework/result.php
+++ class2/PHP_Session2_Practice_Files/PHP_Session2_Practice_Files_Completed/Homework/result.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link href="assets/style.css" type="text/css" rel="stylesheet"/>
-<title>PHP Class 1 Results</title>
-</head>
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link href="assets/style.css" type="text/css" rel="stylesheet"/>
+        <title>PHP Class 1 Results</title>
+    </head>
 
 <body>
 
@@ -48,7 +48,10 @@
 
 
         </div>
-        <div class="footer"><img src="assets/cc_by.png" alt="Creative Commons Licensed By" title="Creative Commons Licensed By"  height="45"/> Izzy Johnston, in conjunction with Girl Develop It! </div>
+        <div class="footer">
+            <img src="assets/cc_by.png" alt="Creative Commons Licensed By" title="Creative Commons Licensed By"  height="45"/> 
+            Izzy Johnston, in conjunction with Girl Develop It! 
+        </div>
     </div>
 </body>
 </html>
